b5e70b7c1edf7a447ec6dd215cdc34de27a61b79
zsimline <zsimline@163.com>
2019-11-27 18:11:20 +0800

    自动化提交测试

--- commit.js
+++ commit.js
@@ -1,21 +1,39 @@
-const { spawn } = require('child_process');
+#!/usr/bin/env node
+
 const readline = require('readline');
+const { spawnSync } = require('child_process');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const print = function(buffer) {
+  console.log(buffer[1].toString())
+}
 
-const git = spawn('git', ['status']);
-git.stdout.on('data', (data) => {
-  console.log(data.toString());
-
+const run = function() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  // 查询状态
+  const gitStatus = spawnSync('git', ['status']);
+  print(gitStatus.output);
+  
   rl.question('请输入本次提交的标注: ', (answer) => {
     commit(answer);
-  });  
-});
+  });
+}
 
 const commit = function(comments) {
-  spawn('git', ['add','.']);
-  spawn('git', ['commit','-m',comments])
+  // 暂存提交
+  const gitAdd = spawnSync('git', ['add','.']);
+  print(gitAdd.output);
+  
+  // 本地提交
+  const gitCommit = spawnSync('git', ['commit','-m',comments]);
+  print(gitCommit.output);
+
+  // gitPush
+  const gitPush = spawnSync('git', ['push','origin','master']);
+  print(gitPush.output);
 }
+
+run();
